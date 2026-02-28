8c0d8ec3b80f97d802da9d87a0b627fdb174c204
Papa G <florentgerard@sfr.fr>
2026-02-28 10:32:06 +0100

    nettoyage du projet

--- src/components/Divider.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/components/divider.css";
-
-const Divider = () => {
-  return <div className="section-divider thick-divider"></div>;
-};
-
-export default Divider;
