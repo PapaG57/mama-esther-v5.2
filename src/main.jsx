b4051748af294eb7e43b48fafd20eed68197e828
Papa G <florentgerard@sfr.fr>
2026-06-05 19:23:07 +0200

    ajout du code pour intégrer Microsoft Clarity

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "./i18n";
+import { clarity } from "@microsoft/clarity";
+
+// Initialisation de Microsoft Clarity
+const clarityId = import.meta.env.VITE_CLARITY_ID;
+if (clarityId) {
+  clarity.init(clarityId);
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
